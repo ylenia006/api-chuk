a9098d0ecd3f4d41a6cbd3b84defc10fa006411e
ylenia bernardi <yleniabernardi1@gmail.com>
2023-05-06 11:38:23 +0200

    3/4

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,35 @@
-console.log("HELLO")
+document.querySelector("#categories").addEventListener("submit", function(e) {
+    e.preventDefault()
+
+    let categorySelected = document.querySelector(".categoriesSelector[name=categoriesSelector]").value
+    
+    console.log(categorySelected)
+    if (categorySelected === "random") {
+        fetch("https://api.chucknorris.io/jokes/random")
+            .then(thenCallback)
+            .then(finalCallback)
+            .catch(catchCallback)
+    } else {
+        fetch(`https://api.chucknorris.io/jokes/random?category=${categorySelected}`)
+            .then(thenCallback)
+            .then(finalCallback)
+            .catch(catchCallback)
+    }
+}) 
+
+
+function thenCallback(response){
+    if(response.status === 200){
+        return response.json()
+    }
+}
+
+function finalCallback(data) {
+    document.querySelector(".jokeBok").innerHTML = data.value
+
+    document.querySelector(".urlBox").innerHTML =  data.url  
+}
+
+function catchCallback(error) {
+    document.querySelector(".jokeBox").innerHTML = error
+}
